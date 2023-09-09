24bad59132a875a9e9a90c34a5c8272371f4f0c8
Timur Ibragimov <i.timur0701@gmail.com>
2023-09-09 13:07:15 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex justify-center items-center h-screen">
+    <div class="space-x-4">
+      <h1 class="text-3xl mb-4 text-purple-500">
+        How to Install Tailwind Daisyui In Vue 3
+      </h1>
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+      <button class="btn btn-accent">Button</button>
+      <button class="btn btn-ghost">Button</button>
+      <button class="btn btn-link">Button</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts"></script>
+
+<style></style>
